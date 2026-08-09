b40b234f3189deba02f2d56d80c341820d05d76e
Star <252556693+StarTheSus@users.noreply.github.com>
2026-08-09 21:32:13 +0300

    fix(flake.nix): revert the changes to the font

--- flake.nix
+++ flake.nix
@@ -37,28 +37,9 @@
 
               src = ./.;
 
-              # for grub-mkfont
-              nativeBuildInputs = [ pkgs.grub2 ];
-
-              fontSrc = pkgs.fetchurl {
-                url = "https://raw.githubusercontent.com/google/fonts/main/ofl/rajdhani/Rajdhani-Regular.ttf";
-                hash = "sha256-bh/CKKgxglGm5WlQLsV7rB5GVsWC+S9ZzOzEaI4Dm5g=";
-              };
               installPhase = ''
                 mkdir -p $out
                 cp -r CyberGRUB-2077/* $out/
-
-                # --- FONT GENERATION ---
-                # Generate smaller sizes and overwrite the original files in $out
-                grub-mkfont -s 24 -o $out/Rajdhani_Regular_32.pf2 $fontSrc
-                grub-mkfont -s 18 -o $out/Rajdhani_Regular_24.pf2 $fontSrc
-                grub-mkfont -s 14 -o $out/Rajdhani_Regular_18.pf2 $fontSrc
-                grub-mkfont -s 12 -o $out/Rajdhani_Regular_16.pf2 $fontSrc
-
-                sed -i 's/Rajdhani Regular 32/Rajdhani Regular 24/g' $out/theme.txt
-                sed -i 's/Rajdhani Regular 24/Rajdhani Regular 18/g' $out/theme.txt
-                sed -i 's/Rajdhani Regular 18/Rajdhani Regular 14/g' $out/theme.txt
-                sed -i 's/Rajdhani Regular 16/Rajdhani Regular 12/g' $out/theme.txt
 
                 # --- ICON PATCHES ---
                 cp $out/icons/nixos.png $out/icons/submenu.png
